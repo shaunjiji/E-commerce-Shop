78417e35a3e60f6b096a324f0adedd3957d7aca2
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-02 07:50:05 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { data } from "./data.js";
+import { useState } from "react";
+import logo from "./images/logo.svg";
+
+function Navbar() {
+  return;
+  <>
+    <div>
+      <img src={logo} alt="" />
+      <nav>
+        <ul>
+          <li>Collections</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <button></button>
+        </li>
+        <li>
+          <img src={avatar} alt="" />
+        </li>
+      </ul>
+    </div>
+  </>;
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [products, setProducts] = useState(data);
+  return (
+    <>
+      <h1>{data.length}</h1>
+    </>
+  );
 }
 
 export default App;
