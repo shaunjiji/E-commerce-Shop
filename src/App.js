df346f2ea9a2cb146ac4de4f78c42ff10f0adfd5
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-02 08:05:44 +0530

    Add styling to navbar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
 import { data } from "./data.js";
 import { useState } from "react";
 import logo from "./images/logo.svg";
+import avatar from "./images/image-avatar.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function Navbar() {
-  return;
-  <>
-    <div>
-      <img src={logo} alt="" />
-      <nav>
-        <ul>
-          <li>Collections</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </div>
-    <div>
-      <ul>
-        <li>
-          <button></button>
-        </li>
-        <li>
-          <img src={avatar} alt="" />
-        </li>
-      </ul>
-    </div>
-  </>;
+  return (
+    <>
+      <header className="flex items-center justify-between p-8 border-b border-slate-400">
+        <div>
+          <img src={logo} alt="" />
+          <nav>
+            <ul>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <button>
+                <AiOutlineShoppingCart />
+              </button>
+            </li>
+            <li>
+              <img src={avatar} alt="" />
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
 }
 
 function App() {
   const [products, setProducts] = useState(data);
   return (
     <>
-      <h1>{data.length}</h1>
+      <Navbar />
     </>
   );
 }
