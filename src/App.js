24d2235360c50ef0979e60c275da5ab96faffd03
Shaun Jiji <shaunjiji123@gmail.com>
2023-03-02 09:35:26 +0530

    refactor image display container

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import Navbar from "./components/Navbar.js";
 
 function App() {
   const [products, setProducts] = useState(data);
+  const [value, setValue] = useState(0);
+  const { mainImage } = products[value];
+
   return (
     <>
       <Navbar />
-      <section>
-        <img src={mainImage} alt=""></img>
-        {products.map((product) => (
-          <ul key={product.id}>
-            <img src={product.thumbnail} alt="" />
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <article>
+          <img src={mainImage} alt="" className="w-9/12"></img>
+
+          <ul className="flex item-center justify-start gap-3 flex-wrap">
+            {products.map((product, index) => (
+              <li key={product.id} onClick={() => setValue(index)}>
+                <img src={product.thumbnail} alt="" className="w-20" />
+              </li>
+            ))}
           </ul>
-        ))}
+        </article>
+        <article>
+          <h1>Lorem ipsum dolor sit amet</h1>
+        </article>
       </section>
     </>
   );
